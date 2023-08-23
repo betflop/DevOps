3c4cdc777705a3fca147384058e4a8bd91bfc452
pavlyk <pawwlyk52@gmail.com>
2023-08-23 22:03:13 +0300

    config

--- diplom_yandex/terraform/node-group.tf
+++ diplom_yandex/terraform/node-group.tf
@@ -1,9 +1,9 @@
 resource "yandex_kubernetes_node_group" "testgroup123" {
-    cluster_id = yandex_kubernetes_cluster.test2.id
-        name       = "testgroup123"
-        instance_template {
-            name       = "test-{instance.short_id}-{instance_group.id}"
-            platform_id = "standard-v1"
+  cluster_id = yandex_kubernetes_cluster.test2.id
+  name       = "testgroup123"
+  instance_template {
+    name       = "test-{instance.short_id}-{instance_group.id}"
+    platform_id = "standard-v1"
     network_interface {
       nat                = true
       subnet_ids         = ["${yandex_vpc_subnet.k8s-subnet.id}"]
@@ -18,13 +18,13 @@ resource "yandex_kubernetes_node_group" "testgroup123" {
       type = "network-hdd"
       size = 64
     }
-          container_runtime {
-              type = "containerd"
-          }
-        }
-    scale_policy {
-        fixed_scale {
-            size = 1
-        }   
+    container_runtime {
+      type = "containerd"
     }
+  }
+  scale_policy {
+    fixed_scale {
+      size = 1
+    }   
+  }
 }
