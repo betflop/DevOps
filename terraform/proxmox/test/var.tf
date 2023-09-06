ddd21d1e3867e2f207c953e18774dd106db21919
betflop <pawwlyk52@gmail.com>
2023-09-06 10:54:09 +0300

    ansible + terraform

---
+++ terraform/proxmox/test/var.tf
@@ -0,0 +1,9 @@
+variable "ssh_key" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrsrVuhoieehrZfB1t8oeli6F4HgouucdyOsLE+O8FwO8rcOb+W/VNmDWPo1XIXno2QsjstCqA4SjY0IjBo7K0lwS/v6MvDpmrPBqeK4j1IHdl2baaLyh8DD9EVaALUVhdy6/9/yfgOhMLM5HP8RBgcCjQD231IUt8HQFY4dA5Hlg1HMjIVef+Nw1BiicLMeio5j81vfIbRRwdH4saDVu6xDi9Kr9s3DfpEIX487FZcW32BfM+My0NeagOFbK0/D5hQRrX7BgmAGyuWfo1zUPTgtqCXWP9bKdAW9lLp1fpNKgfHs1QbJFoMa2WqT2Xz9f22cz39g0qtjAygN40A6ouEOlzQd4ZlpkxOgzcsrOR9DMVkHzi9waxSz9wlF1KVXKii+Zkww1bOLvZmf6Wsd/pnU28pSZhLK+/3EQ03niqO5eogcTM+0MlT+FM4ygDsEcpVhCqHJoDaZ4gUuKvGI1sHsW79EXOBzkDZA1mN7NLQySXOB8EYPcXSRcGKf5z0gk= pavelkozlov@MacBookPro.local"
+}
+variable "proxmox_host" {
+    default = "prox"
+}
+variable "template_name" {
+    default = "VM 9000"
+}
